062e728808cd62593ddc19cf9c62220ce7f8fa67
Arthur Akbarov <arthur.akbarov@yandex.ru>
2017-08-11 12:03:17 +0300

    delete old files

--- Sublime Text/tweaks/Set_Sublime_to_open_unknown_files.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-SET st3Path=%ProgramFiles%\Sublime Text 3\sublime_text.exe
-
-@reg delete "HKEY_CLASSES_ROOT\Unknown\shell\openas" /f
-@reg add "HKEY_CLASSES_ROOT\Unknown\shell\openas\command" /t REG_EXPAND_SZ /v "" /d "%st3Path% \"%%1\"" /f
